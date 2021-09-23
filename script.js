c57c1156802fc9f70cca4e746bcde10e02045ae5
mihirzalavadiya <mihirzalavadiya@gmail.com>
2021-09-23 16:38:33 +0530

    some changes the code

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ $(document).ready(function() {
 
     // typing animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Devloper", "Android Devloper", "Freelancer"],
+        strings: ["Web Devloper", "Android Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     })
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Devloper", "Android Devloper", "Freelancer"],
+        strings: ["Web Devloper", "Android Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
